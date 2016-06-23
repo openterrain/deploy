8197e9c7efb68c82f485a20493f3cfc8b29870f5
Seth Fitzsimmons <seth@mojodna.net>
2016-06-22 22:24:30 -0700

    S3 + local hillshade method

--- main.py
+++ main.py
@@ -1,116 +1,326 @@
 import copy
 from collections import namedtuple
+from StringIO import StringIO
 
-from matplotlib.colors import LightSource, LinearSegmentedColormap
+import boto3
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
 
 Tile = namedtuple("Tile", "x y z")
 
+
+RAMP = {
+    "red": [(0.0, 60 / 255.0, 60 / 255.0),
+            (1.0, 220 / 255.0, 220 / 255.0)],
+    "green": [(0.0, 75 / 255.0, 75 / 255.0),
+              (1.0, 1.0, 1.0)],
+    "blue": [(0.0, 80 / 255.0, 80 / 255.0),
+             (1.0, 100 / 255.0, 100 / 255.0)],
+    "alpha": [(0.0, 0.4, 0.4),
+              (180 / 255.0, 0.0, 0.0),
+              (1.0, 0.2, 0.2)]
+}
+
+COLORMAP = LinearSegmentedColormap("darkmatter", RAMP)
+
 BUFFER = 2
 SRC_TILE_ZOOM = 14
 SRC_TILE_WIDTH = 512
 SRC_TILE_HEIGHT = 512
-# tile = Tile(82, 178, 9)
-tile = Tile(5252, 11446, 15)
 
 DST_TILE_WIDTH = 256
 DST_TILE_HEIGHT = 256
 DST_BLOCK_SIZE = 256
 
-# calculate these in z14 pixels
-dz = SRC_TILE_ZOOM - tile.z
-x = int(2**dz * tile.x)
-y = int(2**dz * tile.y)
-mx = 2**dz * (tile.x + 1)
-my = 2**dz * (tile.y + 1)
-dx = mx - x
-dy = my - y
-top = (2**SRC_TILE_ZOOM * SRC_TILE_HEIGHT) - 1
+def hillshade(elevation, azdeg=315, altdeg=45, vert_exag=1, dx=1, dy=1, fraction=1.):
+    """
+    Calculates the illumination intensity for a surface using the defined
+    azimuth and elevation for the light source.
+    Imagine an artificial sun placed at infinity in some azimuth and
+    elevation position illuminating our surface. The parts of the surface
+    that slope toward the sun should brighten while those sides facing away
+    should become darker.
+    Parameters
+    ----------
+    elevation : array-like
+        A 2d array (or equivalent) of the height values used to generate an
+        illumination map
+    azdeg : number, optional
+        The azimuth (0-360, degrees clockwise from North) of the light
+        source. Defaults to 315 degrees (from the northwest).
+    altdeg : number, optional
+        The altitude (0-90, degrees up from horizontal) of the light
+        source.  Defaults to 45 degrees from horizontal.
+    vert_exag : number, optional
+        The amount to exaggerate the elevation values by when calculating
+        illumination. This can be used either to correct for differences in
+        units between the x-y coordinate system and the elevation
+        coordinate system (e.g. decimal degrees vs meters) or to exaggerate
+        or de-emphasize topographic effects.
+    dx : number, optional
+        The x-spacing (columns) of the input *elevation* grid.
+    dy : number, optional
+        The y-spacing (rows) of the input *elevation* grid.
+    fraction : number, optional
+        Increases or decreases the contrast of the hillshade.  Values
+        greater than one will cause intermediate values to move closer to
+        full illumination or shadow (and clipping any values that move
+        beyond 0 or 1). Note that this is not visually or mathematically
+        the same as vertical exaggeration.
+    Returns
+    -------
+    intensity : ndarray
+        A 2d array of illumination values between 0-1, where 0 is
+        completely in shadow and 1 is completely illuminated.
+    """
+    # Azimuth is in degrees clockwise from North. Convert to radians
+    # counterclockwise from East (mathematical notation).
+    az = np.radians(90 - azdeg)
+    alt = np.radians(altdeg)
 
-if dz != -1:
-    print("Unmatched zooms aren't supported yet:", dz)
-    exit(1)
+    # Because most image and raster GIS data has the first row in the array
+    # as the "top" of the image, dy is implicitly negative.  This is
+    # consistent to what `imshow` assumes, as well.
+    dy = -dy
 
-# y, x (rows, columns)
-window = [
-          [
-           top - (top - (SRC_TILE_HEIGHT * y)),
-           top - (top - ((SRC_TILE_HEIGHT * y) + int(SRC_TILE_HEIGHT * dy)))
-          ],
-          [
-           SRC_TILE_WIDTH * x,
-           (SRC_TILE_WIDTH * x) + int(SRC_TILE_WIDTH * dx)
-          ]
-         ]
+    # Calculate the intensity from the illumination angle
+    dy, dx = np.gradient(vert_exag * elevation, dy, dx)
+    # The aspect is defined by the _downhill_ direction, thus the negative
+    aspect = np.arctan2(-dy, -dx)
+    slope = 0.5 * np.pi - np.arctan(np.hypot(dx, dy))
+    intensity = (np.sin(alt) * np.sin(slope) +
+                 np.cos(alt) * np.cos(slope) * np.cos(az - aspect))
 
-buffered_window = copy.deepcopy(window)
+    # Apply contrast stretch
+    imin, imax = intensity.min(), intensity.max()
+    intensity *= fraction
 
-# buffer so we have neighboring pixels
-buffered_window[0][0] -= BUFFER
-buffered_window[0][1] += BUFFER
-buffered_window[1][0] -= BUFFER
-buffered_window[1][1] += BUFFER
+    # Rescale to 0-1, keeping range before contrast stretch
+    # If constant slope, keep relative scaling (i.e. flat should be 0.5,
+    # fully occluded 0, etc.)
+    # if (imax - imin) > 1e-6:
+    #     # Strictly speaking, this is incorrect. Negative values should be
+    #     # clipped to 0 because they're fully occluded. However, rescaling
+    #     # in this manner is consistent with the previous implementation and
+    #     # visually appears better than a "hard" clip.
+    #     intensity -= imin
+    #     intensity /= (imax - imin)
+    intensity = np.clip(intensity, 0, 1, intensity)
 
-with rasterio.open("mapzen.xml") as src:
-    data = src.read(1, window=buffered_window, masked=True)
-    dx = abs(src.meta["affine"][0])
-    dy = abs(src.meta["affine"][4])
+    return intensity
 
-    meta = src.meta.copy()
-    del meta["transform"]
-    meta.update(
-        driver='GTiff',
-        height=buffered_window[0][1] - buffered_window[0][0],
-        width=buffered_window[1][1] - buffered_window[1][0],
-        affine=src.window_transform(buffered_window),
-    )
-    with rasterio.open("windowed_src.tif", "w", **meta) as dst:
-        dst.write(data, 1)
+def handle(event, context):
+    print "processing event:", event
+    zoom = int(event["params"]["path"]["z"])
+    x = int(event["params"]["path"]["x"])
+    y, format = event["params"]["path"]["y"].split(".")
+    y = int(y)
+    tile = Tile(x, y, zoom)
 
-    meta = src.meta.copy()
-    del meta["transform"]
+    # TODO bail if format != "png"
 
-    meta.update(
-        driver='GTiff',
-        dtype=rasterio.uint8,
-        compress="deflate",
-        predictor=1,
-        nodata=None,
-        tiled=True,
-        sparse_ok=True,
-        blockxsize=DST_BLOCK_SIZE,
-        blockysize=DST_BLOCK_SIZE,
-        height=DST_TILE_HEIGHT,
-        width=DST_TILE_WIDTH,
-    )
+    # calculate these in z14 pixels
+    dz = SRC_TILE_ZOOM - tile.z
+    x = 2**dz * tile.x
+    y = 2**dz * tile.y
+    mx = 2**dz * (tile.x + 1)
+    my = 2**dz * (tile.y + 1)
+    dx = mx - x
+    dy = my - y
+    top = (2**SRC_TILE_ZOOM * SRC_TILE_HEIGHT) - 1
 
-    meta["affine"] = src.window_transform(window)
+    if dz != -1:
+        # TODO implement zooms other than 15
+        print("Unmatched zooms aren't supported yet:", dz)
+        exit(1)
 
-    ls = LightSource()
-    hs = ls.hillshade(data,
-        dx=dx,
-        dy=dy,
-    )
-    hs = (255.0 * hs).astype(np.uint8)
+    # y, x (rows, columns)
+    window = [
+              [
+               top - (top - (SRC_TILE_HEIGHT * y)),
+               top - (top - ((SRC_TILE_HEIGHT * y) + int(SRC_TILE_HEIGHT * dy)))
+              ],
+              [
+               SRC_TILE_WIDTH * x,
+               (SRC_TILE_WIDTH * x) + int(SRC_TILE_WIDTH * dx)
+              ]
+             ]
 
-    with rasterio.open("windowed.tif", "w", **meta) as dst:
-        # ignore the border pixels when writing
-        dst.write(hs[BUFFER:-BUFFER, BUFFER:-BUFFER], 1)
+    buffered_window = copy.deepcopy(window)
 
-    cdict = {
-        "red": [(0.0, 60 / 255.0, 60 / 255.0),
-                (1.0, 220 / 255.0, 220 / 255.0)],
-        "green": [(0.0, 75 / 255.0, 75 / 255.0),
-                  (1.0, 1.0, 1.0)],
-        "blue": [(0.0, 80 / 255.0, 80 / 255.0),
-                 (1.0, 100 / 255.0, 100 / 255.0)],
-        "alpha": [(0.0, 0.4, 0.4),
-                  (162 / 255.0, 0.0, 0.0),
-                  (1.0, 0.2, 0.2)]
-    }
+    # buffer so we have neighboring pixels
+    buffered_window[0][0] -= BUFFER
+    buffered_window[0][1] += BUFFER
+    buffered_window[1][0] -= BUFFER
+    buffered_window[1][1] += BUFFER
 
-    cmap = LinearSegmentedColormap("darkmatter", cdict)
+    with rasterio.open("mapzen.xml") as src:
+        data = src.read(1, window=buffered_window, masked=True)
+        dx = abs(src.meta["affine"][0])
+        dy = abs(src.meta["affine"][4])
 
-    plt.imsave("windowed_cmap.png", hs, cmap=cmap)
+        # meta = src.meta.copy()
+        # del meta["transform"]
+        # meta.update(
+        #     driver='GTiff',
+        #     height=buffered_window[0][1] - buffered_window[0][0],
+        #     width=buffered_window[1][1] - buffered_window[1][0],
+        #     affine=src.window_transform(buffered_window),
+        # )
+        # with rasterio.open("{}_{}_{}_src.tif".format(tile.z, tile.x, tile.y), "w", **meta) as dst:
+        #     dst.write(data, 1)
+
+        # filter out negative values
+        data[data == src.meta["nodata"]] = 9999
+        data[data < 0] = 0
+        data[data == 9999] = src.meta["nodata"]
+
+        # meta = src.meta.copy()
+        # del meta["transform"]
+        # meta.update(
+        #     driver='GTiff',
+        #     height=buffered_window[0][1] - buffered_window[0][0],
+        #     width=buffered_window[1][1] - buffered_window[1][0],
+        #     affine=src.window_transform(buffered_window),
+        # )
+        # with rasterio.open("{}_{}_{}_src_filtered.tif".format(tile.z, tile.x, tile.y), "w", **meta) as dst:
+        #     dst.write(data, 1)
+
+        hs = hillshade(data,
+            dx=dx,
+            dy=dy,
+        )
+        hs = (255.0 * hs).astype(np.uint8)
+
+        # meta = src.meta.copy()
+        # del meta["transform"]
+        # meta.update(
+        #     driver='GTiff',
+        #     dtype=rasterio.uint8,
+        #     compress="deflate",
+        #     predictor=1,
+        #     nodata=None,
+        #     tiled=True,
+        #     sparse_ok=True,
+        #     blockxsize=DST_BLOCK_SIZE,
+        #     blockysize=DST_BLOCK_SIZE,
+        #     height=DST_TILE_HEIGHT,
+        #     width=DST_TILE_WIDTH,
+        #     affine=src.window_transform(window)
+        # )
+        #
+        # with rasterio.open("{}_{}_{}.tif".format(tile.z, tile.x, tile.y), "w", **meta) as dst:
+        #     # ignore the border pixels when writing
+        #     dst.write(hs[BUFFER:-BUFFER, BUFFER:-BUFFER], 1)
+
+        out = StringIO()
+        plt.imsave(
+            out,
+            hs[BUFFER:-BUFFER, BUFFER:-BUFFER],
+            cmap=COLORMAP,
+            vmin=0,
+            vmax=255,
+            format=format,
+        )
+
+        # plt.imsave(
+        #     "{}_{}_{}.png".format(tile.z, tile.x, tile.y),
+        #     hs[BUFFER:-BUFFER, BUFFER:-BUFFER],
+        #     cmap=COLORMAP,
+        #     vmin=0,
+        #     vmax=255,
+        #     format=format,
+        # )
+
+        s3 = boto3.resource('s3')
+        # TODO make configurable
+        bucket = "hillshades.openterrain.org"
+        key = "{}/{}/{}.{}".format(tile.z, tile.x, tile.y, format)
+        s3.Object(
+            bucket,
+            key,
+        ).put(
+            Body=out.getvalue(),
+            ACL="public-read",
+            ContentType="image/{}".format(format),
+            # TODO
+            CacheControl="",
+            StorageClass="REDUCED_REDUNDANCY",
+        )
+
+        return {
+            "location": "http://{}.s3.amazonaws.com/{}".format(bucket, key)
+        }
+
+if __name__ == "__main__":
+    # handle({
+    #     "params": {
+    #         "path": {
+    #             "z": "15",
+    #             "x": "5252",
+    #             "y": "11445.png",
+    #         }
+    #     }
+    # }, None)
+
+    # # Elliot Bay
+    # handle({
+    #     "params": {
+    #         "path": {
+    #             "z": "15",
+    #             "x": "5244",
+    #             "y": "11444.png",
+    #         }
+    #     }
+    # }, None)
+    #
+    # # improperly tinted?
+    # # 15/5244/11446.png
+    # handle({
+    #     "params": {
+    #         "path": {
+    #             "z": "15",
+    #             "x": "5244",
+    #             "y": "11446.png",
+    #         }
+    #     }
+    # }, None)
+    #
+    # handle({
+    #     "params": {
+    #         "path": {
+    #             "z": "15",
+    #             "x": "5244",
+    #             "y": "11447.png",
+    #         }
+    #     }
+    # }, None)
+    handle({
+        "params": {
+            "path": {
+                "z": "15",
+                "x": "5245",
+                "y": "11441.png",
+            }
+        }
+    }, None)
+    handle({
+        "params": {
+            "path": {
+                "z": "15",
+                "x": "5246",
+                "y": "11441.png",
+            }
+        }
+    }, None)
+    handle({
+        "params": {
+            "path": {
+                "z": "15",
+                "x": "5247",
+                "y": "11441.png",
+            }
+        }
+    }, None)

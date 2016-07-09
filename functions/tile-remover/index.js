b7dfdc3a7d3b82c27029408f0a549591a7b52101
Seth Fitzsimmons <seth@mojodna.net>
2016-07-08 19:58:47 -0700

    Handle messages in bulk, no need to re-invoke

--- functions/tile-remover/index.js
+++ functions/tile-remover/index.js
@@ -1,82 +1,51 @@
 "use strict";
 
-const AWS = require("aws-sdk"),
+const async = require("async"),
+  AWS = require("aws-sdk"),
   env = require("require-env");
 
-const Lambda = new AWS.Lambda(),
-  S3 = new AWS.S3(),
+const S3 = new AWS.S3(),
   SQS = new AWS.SQS();
 
-const QUEUE_URL = env.require("QUEUE_URL"),
-  PROCESS_MESSAGE = "process-message";
+const QUEUE_URL = env.require("QUEUE_URL");
 
-
-function process(message, callback) {
-  const payload = JSON.parse(message.Body);
-
-  return S3.deleteObject({
-    Bucket: payload.Bucket,
-    Key: payload.Key,
-  }, (err) => {
-    if (err) {
-      console.warn(err.stack);
-    }
-
-    // delete message
-    const params = {
-      QueueUrl: QUEUE_URL,
-      ReceiptHandle: message.ReceiptHandle
-    };
-    return SQS.deleteMessage(params, (err) => callback(err, message));
-  });
-}
-
-function poll(functionName, callback) {
-  const params = {
-    QueueUrl: QUEUE_URL,
-    MaxNumberOfMessages: 10, // TODO appears to assume 10 messages / polling interval
-    VisibilityTimeout: 10
-  };
-  // batch request messages
-  SQS.receiveMessage(params, (err, data) => {
-    if (err) {
-      return callback(err);
-    }
-
-    // for each message, reinvoke the function
-    const promises = (data.Messages || []).map((message) => {
-      const payload = {
-        operation: PROCESS_MESSAGE,
-        message: message
-      };
-      const params = {
-        FunctionName: functionName,
-        InvocationType: "Event",
-        Payload: new Buffer(JSON.stringify(payload))
-      };
-      return new Promise((resolve, reject) => {
-        Lambda.invoke(params, (err) => err ? reject(err) : resolve());
-      });
-    });
-    // complete when all invocations have been made
-    Promise.all(promises).then(() => {
-      const result = `Messages received: ${data.Messages.length}`;
-      console.log(result);
-      callback(null, result);
-    });
-  });
-}
 
 exports.handle = (event, context, callback) => {
-  try {
-    if (event.operation === PROCESS_MESSAGE) {
-      // invoked by poller
-      process(event.message, callback);
-    } else {
-      // invoked by schedule
-      poll(context.functionName, callback);
-    }
-  } catch (err) {
-    callback(err);
-  }
+  return async.doWhilst(
+    done => {
+      // batch request messages
+      return SQS.receiveMessage({
+        QueueUrl: QUEUE_URL,
+        MaxNumberOfMessages: 10, // maximum number of messages that can be fetched
+        VisibilityTimeout: 10
+      }, (err, data) => {
+        if (err) {
+          return done(err);
+        }
+
+        return async.each(data.Messages, (message, cb) => {
+          const payload = JSON.parse(message.Body);
+
+          console.log("deleting", payload.Key);
+
+          return S3.deleteObject({
+            Bucket: payload.Bucket,
+            Key: payload.Key,
+          }, (err) => {
+            if (err) {
+              console.warn(err.stack);
+            }
+
+            // delete message
+            return SQS.deleteMessage({
+              QueueUrl: QUEUE_URL,
+              ReceiptHandle: message.ReceiptHandle
+            }, cb);
+          });
+        }, err => done(err, (data.Messages || []).length));
+      });
+    },
+    messageCount => messageCount > 0 && context.getRemainingTimeInMillis() > 1000,
+    callback
+  );
 };

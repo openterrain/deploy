66b5df40aa3b63e9e1955143dc3e100e5df92a6e
Seth Fitzsimmons <seth@mojodna.net>
2016-07-08 18:55:10 -0700

    Remove logging

--- functions/randomized-redirector/index.js
+++ functions/randomized-redirector/index.js
@@ -6,8 +6,6 @@ const HOSTS = env.require("HOSTS").split(" "),
   PROTOCOL = process.env.PROTOCOL || "http:";
 
 exports.handle = (event, context, callback) => {
-  console.log("Event:", event);
-
   const path = event.params.path,
     targetPath = [path.style, path.z, path.x, path.y].join("/");
 

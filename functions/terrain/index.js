5931bffddee0c64f04e6279bf07a22e357f450a7
Seth Fitzsimmons <seth@mojodna.net>
2016-07-10 11:49:59 -0700

    Specify maxzoom to prevent extraneous requests

--- functions/terrain/index.js
+++ functions/terrain/index.js
@@ -10,7 +10,15 @@ const SOURCE = {
         source: "http://tile.stamen.com.s3-website-us-east-1.amazonaws.com/terrain-bg/{z}/{x}/{y}.png",
       },
       {
-        source: "http://hillshades.openterrain.org.s3-website-us-east-1.amazonaws.com/positron/{z}/{x}/{y}.png",
+        source: {
+          protocol: "http:",
+          host: "hillshades.openterrain.org.s3-website.us-east-1.amazonaws.com",
+          pathname: "/positron/{z}/{x}/{y}.png",
+          info: {
+            minzoom: 0,
+            maxzoom: 15
+          }
+        },
         "comp-op": "hard-light",
         opacity: "0.6"
       },

0aecbcdaf62dacd6bb4ed434bd7f77ad5a5d3266
Seth Fitzsimmons <seth@mojodna.net>
2016-07-19 14:59:23 -0700

    Correct symlinks

--- functions/terrain-classic-water/index.js
+++ functions/terrain-classic-water/index.js
@@ -1,11 +1,1 @@
-"use strict";
-
-const env = require("require-env");
-
-const makeHandler = require("./lib/");
-
-exports.handle = makeHandler(
-  env.require("SOURCE_URI"),
-  env.require("S3_BUCKET"),
-  env.require("KEY_PREFIX")
-);
+../../handler.js
